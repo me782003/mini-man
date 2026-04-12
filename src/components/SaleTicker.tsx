0bfd892f38bd18ee48b2b6f0294af3638312028a
me782003 <me782003f@gmail.com>
2026-04-13 00:17:12 +0200

    finish home page responsive 2

--- src/components/SaleTicker.tsx
+++ src/components/SaleTicker.tsx
@@ -1,33 +1,44 @@
 'use client';
 
+import { usePathname } from "next/navigation";
+
 const items = Array.from({ length: 12 }, (_, i) => i);
 
 export default function SaleTicker() {
-    return (
-        <div className="w-full overflow-hidden bg-[#ff2a2a] bg-gradient-to-b from-[#D10C11] via-[#E23E38] to-[#E7090E] shadow-2xl">
-            <div className="sale-ticker-track flex min-w-max items-center whitespace-nowrap py-3 ">
-                {items.map((item) => (
-                    <div
-                        key={item}
-                        className="mx-6 flex items-center gap-8 text-[14px] font-extrabold uppercase leading-none sm:text-[20px]"
-                    >
-                        <span className="text-white">50% off</span>
-                        <span className="text-black">sale</span>
-                    </div>
-                ))}
 
-                {items.map((item) => (
-                    <div
-                        key={`duplicate-${item}`}
-                        className="mx-6 flex items-center gap-8 text-[14px] font-extrabold uppercase leading-none sm:text-[20px]"
-                    >
-                        <span className="text-white">50% off</span>
-                        <span className="text-black">sale</span>
-                    </div>
-                ))}
-            </div>
 
-            <style jsx>{`
+  const pathname = usePathname();
+
+  const isHome = pathname === "/en" || pathname === "/ar";
+
+
+
+
+  return (
+    <div className={`w-full   overflow-hidden bg-[#ff2a2a] bg-gradient-to-b from-[#D10C11] via-[#E23E38] to-[#E7090E] shadow-2xl ${isHome ? " md:!hidden" : ""}`}>
+      <div className="sale-ticker-track flex min-w-max items-center whitespace-nowrap py-3 ">
+        {items.map((item) => (
+          <div
+            key={item}
+            className="mx-6 flex items-center gap-8 text-[14px] font-extrabold uppercase leading-none sm:text-[20px]"
+          >
+            <span className="text-white">50% off</span>
+            <span className="text-black">sale</span>
+          </div>
+        ))}
+
+        {items.map((item) => (
+          <div
+            key={`duplicate-${item}`}
+            className="mx-6 flex items-center gap-8 text-[14px] font-extrabold uppercase leading-none sm:text-[20px]"
+          >
+            <span className="text-white">50% off</span>
+            <span className="text-black">sale</span>
+          </div>
+        ))}
+      </div>
+
+      <style jsx>{`
         .sale-ticker-track {
           animation: saleTicker 100s linear infinite;
         }
@@ -41,6 +52,6 @@ export default function SaleTicker() {
           }
         }
       `}</style>
-        </div>
-    );
+    </div>
+  );
 }
